7067a11861c4f162e7a2d62d07a85e26b0910fd0
kurtmclester <kurtmclester@users.noreply.github.com>
2016-10-01 12:28:15 +0100

    Add techtimes.com

---
+++ services/web_techtimes_com.py
@@ -0,0 +1,7 @@
+refresh = 4
+version = 20161001.01
+
+urls = ['http://www.techtimes.com/rss/archives/all.xml']
+regex = [r'^https?:\/\/[^\/]*techtimes\.com\/']
+videoregex = [r'/video/']
+liveregex = []
