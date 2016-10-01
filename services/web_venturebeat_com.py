2d1ec490775b1312193e961fe9f6cc43bde92b07
kurtmclester <kurtmclester@users.noreply.github.com>
2016-10-01 14:13:28 +0100

    add VentureBeat

---
+++ services/web_venturebeat_com.py
@@ -0,0 +1,7 @@
+refresh = 5
+version = 20161001.02
+
+urls = ['http://feeds.feedburner.com/venturebeat/SZYF?format=xml']
+regex = [r'^https?:\/\/[^\/]*venturebeat\.com\/']
+videoregex = []
+liveregex = []
