ae8d4038b72e2a2f529cca9b7fe28df4cfa0f0dd
kurtmclester <kurtmclester@users.noreply.github.com>
2016-10-05 21:17:55 +0100

    Delete web__elkhabar_com.py

--- services/web__elkhabar_com.py
+++
@@ -1,9 +0,0 @@
-refresh = 5
-version = 20160326.02
-
-urls = ['http://www.elkhabar.com',
-	'http://www.elkhabar.com/feeds/']
-regex = [r'^https?:\/\/[^\/]*elkhabar\.com']
-videoregex = []
-liveregex = []
-
